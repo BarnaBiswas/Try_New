d7535e04b9838e672b9625c4712b835997dcb5f6
Barna <barnabiswas99@gmail.com>
2020-04-10 11:51:38 +0600

    user and post

--- app/src/main/java/com/example/welink/Controller/MainActivity.java
+++ app/src/main/java/com/example/welink/Controller/MainActivity.java
@@ -1,12 +1,11 @@
-package com.example.welink;
+package com.example.welink.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import com.example.welink.R;
 
 public class MainActivity extends AppCompatActivity {
     Button login,signup;
